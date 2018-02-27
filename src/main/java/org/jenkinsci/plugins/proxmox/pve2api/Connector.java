42202b53210416e061ac603bfe62eb3d5238d209
Maxhy <maxime@islog.com>
2018-02-27 12:53:12 +0100

    Fixes to work with Jenkins 2.89.4

--- src/main/java/org/jenkinsci/plugins/proxmox/pve2api/Connector.java
+++ src/main/java/org/jenkinsci/plugins/proxmox/pve2api/Connector.java
@@ -44,7 +44,7 @@ public class Connector {
         TrustManager trm = new X509TrustManager() {
             public void checkClientTrusted(X509Certificate[] certs, String authType) {}
             public void checkServerTrusted(X509Certificate[] certs, String authType) {}
-            public X509Certificate[] getAcceptedIssuers() {	return null; }
+            public X509Certificate[] getAcceptedIssuers() {    return null; }
         };
 
         HostnameVerifier hnv = new HostnameVerifier() {
