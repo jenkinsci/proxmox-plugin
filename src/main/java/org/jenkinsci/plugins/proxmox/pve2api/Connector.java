a4713850b52abc0b9e2421ab63b85ea90144015d
Bruno Verachten <gounthar@gmail.com>
2025-04-24 13:55:31 +0200

    refactor(connector): Clean up imports and improve code formatting in Connector.java

--- src/main/java/org/jenkinsci/plugins/proxmox/pve2api/Connector.java
+++ src/main/java/org/jenkinsci/plugins/proxmox/pve2api/Connector.java
@@ -1,25 +1,22 @@
 package org.jenkinsci.plugins.proxmox.pve2api;
 
+import hudson.util.Secret;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Logger;
 import java.util.logging.Level;
-
+import java.util.logging.Logger;
 import javax.security.auth.login.LoginException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import kong.unirest.Unirest;
-import kong.unirest.UnirestInstance;
 import kong.unirest.HttpRequest;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
-import kong.unirest.json.JSONObject;
+import kong.unirest.Unirest;
+import kong.unirest.UnirestInstance;
 import kong.unirest.json.JSONArray;
-
-import hudson.util.Secret;
+import kong.unirest.json.JSONObject;
 
 public class Connector {
 
@@ -34,9 +31,9 @@ public class Connector {
     private String authTicket;
     private Date authTicketIssuedTimestamp;
     private String csrfPreventionToken;
-	private UnirestInstance unirest;
-	
-	private static final Logger LOGGER = Logger.getLogger(Connector.class.getName());
+    private UnirestInstance unirest;
+
+    private static final Logger LOGGER = Logger.getLogger(Connector.class.getName());
 
     public Connector(String hostname, String username, String realm, Secret password) {
         this(hostname, username, realm, password, false);
@@ -44,70 +41,69 @@ public class Connector {
 
     public Connector(String hostname, String username, String realm, Secret password, Boolean ignoreSSL) {
         this.port = 8006;
-		// Parse hostname for port information
-		try {
-			URI uri = new URI("https://" + hostname);
-			if (uri.getPort() != -1) {
-				hostname = uri.getHost();
-				port = uri.getPort();
-			}
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
+        // Parse hostname for port information
+        try {
+            URI uri = new URI("https://" + hostname);
+            if (uri.getPort() != -1) {
+                hostname = uri.getHost();
+                port = uri.getPort();
+            }
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
         this.username = username;
         this.realm = realm;
         this.password = password;
-		
-		this.unirest = Unirest.spawnInstance();
-		unirest.config().verifySsl(!ignoreSSL).reset();
-        
+
+        this.unirest = Unirest.spawnInstance();
+        unirest.config().verifySsl(!ignoreSSL).reset();
+
         this.authTicketIssuedTimestamp = null;
         this.baseURL = "https://" + hostname + ":" + port.toString() + "/api2/json/";
     }
 
     public void login() throws LoginException {
         JSONObject authTickets = unirest.post(baseURL + "access/ticket")
-			.field("username", username + "@" + realm)
-			.field("password", password.getPlainText())
-			.asJson()
-			.getBody()
-			.getObject();
+                .field("username", username + "@" + realm)
+                .field("password", password.getPlainText())
+                .asJson()
+                .getBody()
+                .getObject();
         try {
-			JSONObject data = authTickets.getJSONObject("data");
+            JSONObject data = authTickets.getJSONObject("data");
             authTicket = data.get("ticket").toString();
             csrfPreventionToken = data.get("CSRFPreventionToken").toString();
             authTicketIssuedTimestamp = new Date();
         } catch (Exception e) {
-			LOGGER.log(Level.SEVERE, "Failed reading JSON response", e);
+            LOGGER.log(Level.SEVERE, "Failed reading JSON response", e);
             throw new LoginException("Failed reading JSON response");
         }
     }
 
     public void checkIfAuthTicketIsValid() throws LoginException {
-        //Authentication ticket has a lifetime of 2 hours, so login again when it expires
+        // Authentication ticket has a lifetime of 2 hours, so login again when it expires
         if (authTicketIssuedTimestamp == null
                 || authTicketIssuedTimestamp.getTime() <= (new Date().getTime() - (120 * 60 * 1000))) {
             login();
         }
     }
-	
-	private HttpResponse<JsonNode> JSONResource(HttpRequest req) throws LoginException {
-		checkIfAuthTicketIsValid();
-		return req.header("Cookie", "PVEAuthCookie=" + authTicket)
-        .header("CSRFPreventionToken", csrfPreventionToken)
-		.asJson();
-	}
+
+    private HttpResponse<JsonNode> JSONResource(HttpRequest req) throws LoginException {
+        checkIfAuthTicketIsValid();
+        return req.header("Cookie", "PVEAuthCookie=" + authTicket)
+                .header("CSRFPreventionToken", csrfPreventionToken)
+                .asJson();
+    }
 
     private JsonNode getJSONResource(String apiUrl) throws LoginException {
-        return JSONResource(unirest.get(baseURL + apiUrl))
-		.getBody();
+        return JSONResource(unirest.get(baseURL + apiUrl)).getBody();
     }
-	
-	private JsonNode postJSONResource(String apiUrl, String body) throws LoginException {
+
+    private JsonNode postJSONResource(String apiUrl, String body) throws LoginException {
         return JSONResource(unirest.post(baseURL + apiUrl)
-			.header("Content-Type", "application/x-www-form-urlencoded")
-			.body(body))
-		.getBody();
+                        .header("Content-Type", "application/x-www-form-urlencoded")
+                        .body(body))
+                .getBody();
     }
 
     public List<String> getNodes() throws LoginException {
@@ -123,19 +119,19 @@ public class Connector {
         JsonNode response = getJSONResource("nodes/" + node + "/tasks/" + taskId + "/status");
         return response.getObject().getJSONObject("data");
     }
-    
+
     public JSONObject getQemuMachineStatus(String node, Integer vmid) throws LoginException {
-      JsonNode response = getJSONResource("nodes/" + node + "/qemu/" + vmid + "/status/current");
-      return response.getObject().getJSONObject("data");
+        JsonNode response = getJSONResource("nodes/" + node + "/qemu/" + vmid + "/status/current");
+        return response.getObject().getJSONObject("data");
     }
-    
+
     public Boolean isQemuMachineRunning(String node, Integer vmid) throws LoginException {
-      JSONObject QemuMachineStatus = null;
-      Boolean isRunning = true;
-      QemuMachineStatus = getQemuMachineStatus(node, vmid);
-      isRunning = (QemuMachineStatus.getString("status").equals("running"));
-      return isRunning;
-  }
+        JSONObject QemuMachineStatus = null;
+        Boolean isRunning = true;
+        QemuMachineStatus = getQemuMachineStatus(node, vmid);
+        isRunning = (QemuMachineStatus.getString("status").equals("running"));
+        return isRunning;
+    }
 
     public JSONObject waitForTaskToFinish(String node, String taskId) throws LoginException, InterruptedException {
         JSONObject lastTaskStatus = null;
@@ -152,7 +148,8 @@ public class Connector {
 
     public HashMap<String, Integer> getQemuMachines(String node) throws LoginException {
         HashMap<String, Integer> res = new HashMap<String, Integer>();
-        JSONArray qemuVMs = getJSONResource("nodes/" + node + "/qemu").getObject().getJSONArray("data");
+        JSONArray qemuVMs =
+                getJSONResource("nodes/" + node + "/qemu").getObject().getJSONArray("data");
         for (int i = 0; i < qemuVMs.length(); i++) {
             JSONObject vm = qemuVMs.getJSONObject(i);
             res.put(vm.getString("name"), vm.getInt("vmid"));
@@ -163,7 +160,8 @@ public class Connector {
     public List<String> getQemuMachineSnapshots(String node, Integer vmid) throws LoginException {
         List<String> res = new ArrayList<String>();
         JSONArray snapshots = getJSONResource("nodes/" + node + "/qemu/" + vmid.toString() + "/snapshot")
-                .getObject().getJSONArray("data");
+                .getObject()
+                .getJSONArray("data");
         for (int i = 0; i < snapshots.length(); i++) {
             res.add(snapshots.getJSONObject(i).getString("name"));
         }
@@ -171,31 +169,31 @@ public class Connector {
     }
 
     public String rollbackQemuMachineSnapshot(String node, Integer vmid, String snapshotName) throws LoginException {
-        return postJSONResource("nodes/" + node + "/qemu/" + vmid.toString() + "/snapshot/" + snapshotName + "/rollback", "")
-			.getObject()
-			.getString("data");
+        return postJSONResource(
+                        "nodes/" + node + "/qemu/" + vmid.toString() + "/snapshot/" + snapshotName + "/rollback", "")
+                .getObject()
+                .getString("data");
     }
 
     public String startQemuMachine(String node, Integer vmid) throws LoginException {
         return postJSONResource("nodes/" + node + "/qemu/" + vmid.toString() + "/status/start", "")
-			.getObject()
-			.getString("data");
+                .getObject()
+                .getString("data");
     }
 
     public String stopQemuMachine(String node, Integer vmid) throws LoginException {
         return postJSONResource("nodes/" + node + "/qemu/" + vmid.toString() + "/status/stop", "")
-			.getObject()
-			.getString("data");
+                .getObject()
+                .getString("data");
     }
-    
-    public String shutdownQemuMachine(String node, Integer vmid) throws LoginException {
-		return postJSONResource("nodes/" + node + "/qemu/" + vmid.toString() + "/status/shutdown", "")
-			.getObject()
-			.getString("data");
-    }
-	
-	 protected void finalize() {
-		 unirest.shutDown();
-	 }
 
+    public String shutdownQemuMachine(String node, Integer vmid) throws LoginException {
+        return postJSONResource("nodes/" + node + "/qemu/" + vmid.toString() + "/status/shutdown", "")
+                .getObject()
+                .getString("data");
+    }
+
+    protected void finalize() {
+        unirest.shutDown();
+    }
 }
