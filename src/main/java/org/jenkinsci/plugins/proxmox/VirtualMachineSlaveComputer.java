2f0cc17a2e78e513f8e47675d031c89fb7d9afe9
Bruno Verachten <gounthar@gmail.com>
2025-04-24 13:56:50 +0200

    refactor(slave): Clean up imports and improve code formatting in VirtualMachineSlaveComputer.java

--- src/main/java/org/jenkinsci/plugins/proxmox/VirtualMachineSlaveComputer.java
+++ src/main/java/org/jenkinsci/plugins/proxmox/VirtualMachineSlaveComputer.java
@@ -1,36 +1,35 @@
 package org.jenkinsci.plugins.proxmox;
 
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicBoolean;
-
 import hudson.model.Executor;
 import hudson.model.Node;
 import hudson.model.Queue;
 import hudson.model.Slave;
 import hudson.slaves.OfflineCause;
 import hudson.slaves.SlaveComputer;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class VirtualMachineSlaveComputer extends SlaveComputer {
 
-	private AtomicBoolean isRevertingSnapshot = new AtomicBoolean(false);
+    private AtomicBoolean isRevertingSnapshot = new AtomicBoolean(false);
 
     public VirtualMachineSlaveComputer(Slave slave) {
         super(slave);
     }
 
     @Override
-	public void tryReconnect() {
-    	if(isRevertingSnapshot.get()) {
-    		getListener().getLogger().println("INFO: trying to reconnect while snapshot revert - ignoring");
-    		return;
-    	}
+    public void tryReconnect() {
+        if (isRevertingSnapshot.get()) {
+            getListener().getLogger().println("INFO: trying to reconnect while snapshot revert - ignoring");
+            return;
+        }
 
-		super.tryReconnect();
-	}
+        super.tryReconnect();
+    }
 
-	@Override
+    @Override
     public void taskAccepted(Executor executor, Queue.Task task) {
         super.taskAccepted(executor, task);
 
@@ -40,33 +39,36 @@ public class VirtualMachineSlaveComputer extends SlaveComputer {
             final VirtualMachineSlave slave = (VirtualMachineSlave) node;
             final VirtualMachineLauncher launcher = (VirtualMachineLauncher) slave.getLauncher();
 
-            if (launcher.isLaunchSupported() && (slave.getRevertPolicy() == VirtualMachineLauncher.RevertPolicy.BEFORE_JOB)) {
+            if (launcher.isLaunchSupported()
+                    && (slave.getRevertPolicy() == VirtualMachineLauncher.RevertPolicy.BEFORE_JOB)) {
                 try {
-                	isRevertingSnapshot.set(true);
+                    isRevertingSnapshot.set(true);
 
-                	final Future<?> disconnectFuture = disconnect(OfflineCause.create(Messages._VirtualMachineSlaveComputer_disconnectBeforeSnapshotRevert()));
-                	disconnectFuture.get();
-                	getListener().getLogger().println("INFO: agent disconnected");
+                    final Future<?> disconnectFuture = disconnect(OfflineCause.create(
+                            Messages._VirtualMachineSlaveComputer_disconnectBeforeSnapshotRevert()));
+                    disconnectFuture.get();
+                    getListener().getLogger().println("INFO: agent disconnected");
 
-                	launcher.revertSnapshot(this, getListener());
-                	getListener().getLogger().println("INFO: snapshot reverted");
+                    launcher.revertSnapshot(this, getListener());
+                    getListener().getLogger().println("INFO: snapshot reverted");
 
-                	launcher.launch(this, getListener());
-                	getListener().getLogger().println("INFO: agent launched");
+                    launcher.launch(this, getListener());
+                    getListener().getLogger().println("INFO: agent launched");
                 } catch (IOException | InterruptedException e) {
-                	getListener().getLogger().println("ERROR: Snapshot revert failed: " + e.getMessage());
+                    getListener().getLogger().println("ERROR: Snapshot revert failed: " + e.getMessage());
                 } catch (ExecutionException e) {
-                	getListener().getLogger().println("ERROR: Exception while performing asynchronous disconnect: " + e.getMessage());
+                    getListener()
+                            .getLogger()
+                            .println("ERROR: Exception while performing asynchronous disconnect: " + e.getMessage());
                 } finally {
-                	isRevertingSnapshot.set(false);
-				}
+                    isRevertingSnapshot.set(false);
+                }
             }
         }
     }
-    
+
     @Override
     protected Future<?> _connect(boolean forceReconnect) {
         return super._connect(forceReconnect);
     }
-
 }
